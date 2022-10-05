9767cf308599885e3ba9fe0e1d3eb23e72656018
Israel Sousa <isckosta@live.com>
2022-10-05 14:51:40 -0300

    Adicionado comandos DailyQuote e configuração da schedule para o Kernel do Laravel

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -1,32 +1,38 @@
 <?php
 
-namespace App\Console;
+    namespace App\Console;
 
-use Illuminate\Console\Scheduling\Schedule;
-use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+    use Illuminate\Console\Scheduling\Schedule;
+    use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+    use Illuminate\Support\Facades\DB;
 
-class Kernel extends ConsoleKernel
-{
-    /**
-     * Define the application's command schedule.
-     *
-     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
-     * @return void
-     */
-    protected function schedule(Schedule $schedule)
+    class Kernel extends ConsoleKernel
     {
-        // $schedule->command('inspire')->hourly();
-    }
 
-    /**
-     * Register the commands for the application.
-     *
-     * @return void
-     */
-    protected function commands()
-    {
-        $this->load(__DIR__.'/Commands');
+        protected $commands = [Commands\DailyQuote::class,];
 
-        require base_path('routes/console.php');
+        /**
+         * Define the application's command schedule.
+         *
+         * @param \Illuminate\Console\Scheduling\Schedule $schedule
+         * @return void
+         */
+        protected function schedule(Schedule $schedule)
+        {
+            // $schedule->command('inspire')->hourly();
+            $schedule->command('quote:daily')
+                ->everyMinute();
+        }
+
+        /**
+         * Register the commands for the application.
+         *
+         * @return void
+         */
+        protected function commands()
+        {
+            $this->load(__DIR__ . '/Commands');
+
+            require base_path('routes/console.php');
+        }
     }
-}
